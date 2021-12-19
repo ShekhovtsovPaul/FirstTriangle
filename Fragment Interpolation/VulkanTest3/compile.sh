4af9f5332cc2402e2da902510f7010ed726f3f00
ShekhovtsovPaul <paulshekhovtsov@gmail.com>
2021-12-19 11:46:08 +0200

    VULKAN triangle

---
+++ Fragment Interpolation/VulkanTest3/compile.sh
@@ -0,0 +1,2 @@
+/Users/paulshekhovtsov/VulkanSDK/macOS/bin/glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
+/Users/paulshekhovtsov/VulkanSDK/macOS/bin/glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
